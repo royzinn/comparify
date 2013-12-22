9446735251590ef940f140a835aa25e0d3bf839f
Roy Zinn <roy.zinn@gmail.com>
2013-12-22 23:52:37 +0200

    topics controller + views

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,9 @@
 class TopicsController < ApplicationController
   before_action :set_topic, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @topics = Topic.all
+  end
   def new
     @topic = Topic.new
   end
@@ -9,6 +12,28 @@ class TopicsController < ApplicationController
   end
 
   def show
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+    if @topic.save
+      redirect_to @topic, notice: 'Topic was successfully created.'
+    else
+      render action: 'new'
+    end
+  end
+
+  def update
+    if @topic.update(pin_params)
+      redirect_to @topic, notice: 'Topic was successfully updated.'
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    @topic.destroy
+    redirect_to topics_url
   end
 
   private
