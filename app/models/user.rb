26a356c1171a128f4e62b252f246b722f6dac1bd
Roy Zinn <roy.zinn@gmail.com>
2014-01-28 14:36:54 +0200

    handling users + following users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,25 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  extend FriendlyId
+  friendly_id :name, use: :slugged
+
   has_many :topics
   has_many :answers
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_ralationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_ralationships
+
+  def following?(other_user)
+    followed_users.include?(other_user)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,33 @@
 require 'spec_helper'
 
 describe User do
-  it {should have_many(:topics)}
-  it {should have_many(:answers)}
+  before { @user = Fabricate(:user) }
+  subject { @user }
+
+  it { should have_many(:topics) }
+  it { should have_many(:answers) }
+  it { should have_many(:relationships) }
+  it { should have_many(:followed_users) }
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
+
+  describe "following" do
+    let!(:other_user) { Fabricate(:user) }
+    before { @user.follow!(other_user) }
+
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+
+    describe "followed user" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
+    end
+
+    describe "and unfollowing" do
+      before { @user.unfollow!(other_user) }
+
+      it { should_not be_following(other_user) }
+      its(:followed_users) { should_not include(other_user) }
+    end
+  end
 end
