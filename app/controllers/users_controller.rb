65916448b7e6e225ed1f228dcc3aac570ab077c1
Roy Zinn <roy.zinn@gmail.com>
2014-02-05 09:08:14 +0200

    fetching activities both as owner and as recipient

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.friendly.find(params[:id])
-    @activities = PublicActivity::Activity.order("created_at DESC").where(owner_type: "User", owner_id: current_user).all
+    @activities = PublicActivity::Activity.order("created_at DESC").where('(owner_type = "User" and owner_id = :user) or (recipient_type = "User" and recipient_id = :user)', {user: @user}).all
   end
 
 end
