5e48c3aeaa81f8ee872529f6c9019b3cee14f040
Roy Zinn <roy.zinn@gmail.com>
2014-02-11 21:42:13 +0200

    trying to add target=_blank to links in answers

--- app/helpers/format_helper.rb
+++ app/helpers/format_helper.rb
@@ -1,15 +1,17 @@
 module FormatHelper
   def format(text)
     context = {
-      asset_root: 'https://a248.e.akamai.net/assets.github.com/images/icons/'
+      asset_root: 'https://a248.e.akamai.net/assets.github.com/images/icons/',
+      link_attr: 'target="_blank"',
+      gfm: true
     }
 
     pipeline = HTML::Pipeline.new [
       HTML::Pipeline::MarkdownFilter,
       HTML::Pipeline::SanitizationFilter,
-      HTML::Pipeline::AutolinkFilter,
-      HTML::Pipeline::EmojiFilter
-    ], context, Hash
+      HTML::Pipeline::EmojiFilter,
+      HTML::Pipeline::AutolinkFilter
+    ], context
 
     pipeline.call(text)[:output].to_s
   end
